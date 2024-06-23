a6fdf079932b234e7c259aaa1e043d03aa6c56da
JorgenDIF <jorgenjonsson80@gmail.com>
2024-06-23 12:54:07 +0200

    feat: add temporary landing page to API and update pagination settings
    - Added a temporary landing page to the API.
    - Updated settings.py to include pagination with a maximum of 10 posts per page.

--- restinease/settings.py
+++ restinease/settings.py
@@ -29,8 +29,13 @@ REST_FRAMEWORK = {
         'rest_framework.authentication.SessionAuthentication'
         if 'DEV' in os.environ
         else 'dj_rest_auth.jwt_auth.JWTCookieAuthentication'
-    )]
+    )],
+    'DEFAULT_PAGINATION_CLASS':
+        'rest_framework.pagination.PageNumberPagination',
+    'PAGE_SIZE': 10,
+    'DATETIME_FORMAT': '%d %b %Y',
 }
+
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
