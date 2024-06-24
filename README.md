6848378174d1a44efe1c935a10b02616f22a4c64
JorgenDIF <jorgenjonsson80@gmail.com>
2024-06-24 19:31:46 +0200

    chore: update settings.py for deployment, add CI database, and add Procfile
    - Updated settings.py with configurations for deployment.
    - Added configuration for CI database.
    - Added Procfile for deployment.

--- README.md
+++ README.md
@@ -1,24 +1,25 @@
 # The API for Rest in Ease
 
-
 ## Table of Contents
-	1.	Introduction
-	2.	Installation
-	3.	Setup
-	4.	API Endpoints
-	•	Authentication
-	•	Users
-	•	Posts
-	•	Comments
-	•	Likes
-	•	Followers
-	5.	Pagination
-	6.	Filtering and Searching
-	7.	Examples
-	•	Authentication Examples
-	•	CRUD Examples
-	8.	Error Handling
-	9.	Testing
-	10.	Deployment
-	11.	Contributing
-	12.	License
+
+1. [Introduction](#introduction)
+2. [Installation](#installation)
+3. [Setup](#setup)
+4. [API Endpoints](#api-endpoints)
+    - [Authentication](#authentication)
+    - [Users](#users)
+    - [Posts](#posts)
+    - [Comments](#comments)
+    - [Likes](#likes)
+    - [Followers](#followers)
+5. [Pagination](#pagination)
+6. [Filtering and Searching](#filtering-and-searching)
+7. [Examples](#examples)
+    - [Authentication Examples](#authentication-examples)
+    - [CRUD Examples](#crud-examples)
+8. [Error Handling](#error-handling)
+9. [Testing](#testing)
+10. [Deployment](#deployment)
+11. [Contributing](#contributing)
+12. [License](#license)
+
--- restinease/settings.py
+++ restinease/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 
 from pathlib import Path
 import os
+import dj_database_url
 
 if os.path.exists('env.py'):
     import env
@@ -36,16 +37,38 @@ REST_FRAMEWORK = {
     'DATETIME_FORMAT': '%d %b %Y',
 }
 
+REST_USE_JWT = True
+JWT_AUTH_SECURE = True
+JWT_AUTH_COOKIE = 'my-app-auth'
+JWT_AUTH_REFRESH_COOKIE = 'my-refresh-token'
+
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-6wb#ip0z2!i%o*pqxqwu4wmwrn)l_y!dnzo_k=#f*@%ttv2e!o'
+SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
+
+# CORS configuration
+# If the environment variable 'CLIENT_ORIGIN' is set, only allow this origin
+# Otherwise, allow localhost during development and the production domain
+
+if 'CLIENT_ORIGIN' in os.environ:
+    CORS_ALLOWED_ORIGINS = [
+        os.environ.get('CLIENT_ORIGIN')
+    ]
+else:
+    CORS_ALLOWED_ORIGINS = [
+        "http://localhost:3000",
+        "http://127.0.0.1:3000",
+        "https://rest-in-ease-api.herokuapp.com"
+
+
+    ]
 
 
 # Application definition
@@ -77,6 +100,7 @@ INSTALLED_APPS = [
 ]
 SITE_ID = 1
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -109,13 +133,18 @@ WSGI_APPLICATION = 'restinease.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
-
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+if 'DEV' in os.environ:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': BASE_DIR / 'db.sqlite3',
+        }
     }
-}
+else:
+    DATABASES = {
+        'default': dj_database_url.parse(os.environ.get("DATABASE_URL"))
+    }
+
 
 
 # Password validation
